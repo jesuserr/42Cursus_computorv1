3bebd6b86cff5c2315aab116ec49b5aeb5df0e9c
jesuserr <ivoice666@hotmail.com>
2024-08-08 18:32:42 +0200

    Plot - Scale adjustments

--- misc/plot_backup.py
+++ misc/plot_backup.py
@@ -34,6 +34,33 @@ def scale_calculator(a, b, c):
     else:
         plt.title('1st Degree Polynomial')
         return SCALE * c / -b, SCALE * c / b
+    
+def plot(coefficients):
+    a, b, c = coefficients[2], coefficients[1], coefficients[0]
+    max_value = max(scale_calculator(a, b, c))
+    min_value = min(scale_calculator(a, b, c))
+    max_value *= SCALE if max_value > 0 else 1 / SCALE
+    min_value *= SCALE if min_value < 0 else 1 / SCALE
+    x_step = (max_value - min_value) / POINTS
+    x_values = []
+    y_values = []
+    for i in range(POINTS):
+        x = min_value + i * x_step
+        x_values.append(x)
+        y = a * x**2 + b * x + c
+        y_values.append(y)
+    #x_values = [min_value + i * x_step for i in range(POINTS)]
+    #y_values = [a * x**2 + b * x + c for x in x_values]
+    plt.plot(x_values, y_values, label=f'{c} + {b}x + {a}x²')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.legend()
+    plt.grid(True)
+    plt.axhline(0, color='black', lw=0.5)
+    plt.axvline(0, color='black', lw=0.5)
+    plt.xlim(x_values[0], x_values[POINTS - 1])
+    plt.show()
+
 
 
 
--- plot.py
+++ plot.py
@@ -7,6 +7,8 @@ def plot(coefficients):
     a, b, c = coefficients[2], coefficients[1], coefficients[0]
     max_value = max(scale_calculator(a, b, c))
     min_value = min(scale_calculator(a, b, c))
+    max_value *= SCALE if max_value > 0 else 1 / SCALE
+    min_value *= SCALE if min_value < 0 else 1 / SCALE
     x_step = (max_value - min_value) / POINTS
     x_values = []
     y_values = []
@@ -15,8 +17,6 @@ def plot(coefficients):
         x_values.append(x)
         y = a * x**2 + b * x + c
         y_values.append(y)
-    #x_values = [min_value + i * x_step for i in range(POINTS)]
-    #y_values = [a * x**2 + b * x + c for x in x_values]
     plt.plot(x_values, y_values, label=f'{c} + {b}x + {a}x²')
     plt.xlabel('x')
     plt.ylabel('y')
@@ -24,6 +24,7 @@ def plot(coefficients):
     plt.grid(True)
     plt.axhline(0, color='black', lw=0.5)
     plt.axvline(0, color='black', lw=0.5)
+    plt.xlim(x_values[0], x_values[POINTS - 1])
     plt.show()
 
 def scale_calculator(a, b, c):
@@ -36,8 +37,8 @@ def scale_calculator(a, b, c):
         if discriminant == 0:
             numerator2 *= -1
         elif discriminant < 0:
-            return SCALE * -b / denominator, SCALE * b / denominator
-        return SCALE * numerator1 / denominator, SCALE * numerator2 / denominator
+            return -b / denominator, b / denominator
+        return numerator1 / denominator, numerator2 / denominator
     else:
         plt.title('1st Degree Polynomial')
-        return SCALE * c / -b, SCALE * c / b
+        return c / -b, c / b
